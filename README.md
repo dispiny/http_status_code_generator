9cf118c4792991ef4e1cfed95abf4c17581b2a05
pjm1024cl <pjm1042cl@gmail.com>
2023-01-13 20:23:32 +0900

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# HTTP Status Code Generator
+
+### How to used?
+Example
+```
+$ curl  http://localhost:5000/status?code=200
+{'message','Status Code has been created.', 'code': 200}
+
+$ curl  http://localhost:5000/status?code=500
+{'message','Status Code has been created.', 'code': 500}
+```
---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, Response
+
+app = Flask(__name__)
+
+@app.route('/status')
+def get():
+    code = request.args.get('code')
+    return Response("{'message','Status Code has been created.', 'code': "+code+"}", status=code, mimetype='application/json')
+
+app.run(host='0.0.0.0', debug=True, port="5000")
